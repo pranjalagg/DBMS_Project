9b7965dbd0cb3a2ab24724ace70dec2d1bdb2680
Pranjal_Win <pranjalaggarwal1999@gmail.com>
2020-11-21 21:46:55 +0530

    3 Tables Script

---
+++ My4Tables.sql
@@ -0,0 +1,91 @@
+-- table 2
+
+INSERT INTO T11_CUSTOMER VALUES (201, 'Paul', 'Walker', '1973-09-12', 'M', 'California', 8937629017, 'paulwalker@actor.com', 33694855, 'Married', 40 );
+INSERT INTO T11_CUSTOMER VALUES (202, 'Jaspal', 'Bhatti','1955-03-03' ,'M','Amritsar', 6710947824 , 'jaspalbhatti@comedian.com', F7922033,'Married', 57);
+INSERT INTO T11_CUSTOMER VALUES (203, 'Lady', 'Diana Spencer','1961-07-01' , 'F','Sandringham', 1111111111 ,'princessidana@wales.com', 125580D, 'Divorced ', 36 );
+INSERT INTO T11_CUSTOMER VALUES (204, 'Drazen', 'Petrovic','1964-10-22' ,'M', 'Croatia', 9178456927, 'drazen@nbabasketball.com', 49105443, 'Single', 28);
+INSERT INTO T11_CUSTOMER VALUES (205, 'James', 'Dean','1931-02-08' ,'M','Indiana', 7826501975 , 'jamesdean@actor.com', 54D68084, 'Single', 24);
+INSERT INTO T11_CUSTOMER VALUES (206, 'John Fitzgerald', 'Kennedy','1917-05-29' ,'M', 'Brookline', 9825908717 , 'jfk@uspresident.com', PD095084, 'Married', 46);
+INSERT INTO T11_CUSTOMER VALUES (207, 'Sam', 'Kinison','1953-12-08' ,'M','Washington', 9027561813 , 'sam@comedian.com', 7A721945, 'Divorced', 38);
+INSERT INTO T11_CUSTOMER VALUES (208, 'Sonu', 'Sood','1973-07-30' ,'M','Moga', 9748632798, 'sonusood@actor.com', BM354070, 'Married', 41);
+INSERT INTO T11_CUSTOMER VALUES (209, 'Barack', 'Obama Sr.','1936-06-18 ','M','Kenya ', 9864276532, 'seniorobama@econimist.com', 8S892341, 'Married', 46);
+INSERT INTO T11_CUSTOMER VALUES (210, 'John', 'Denver','1943-12-31' ,'M','New Mexico', 8816354290 ,'johndenver@singer.com', A7321879, 'Divorced', 53);
+INSERT INTO T11_CUSTOMER VALUES (211, 'Subhash Chandra', 'Bose','1897-01-23' ,'M','Cuttac', 8973526754, 'scbose@freedomfighter.com', H1897385, 'Married', 48);
+INSERT INTO T11_CUSTOMER VALUES (212, 'Kalika Prasad', 'Bhattacharya','1970-09-11' ,'M', 'Silchar', 8927675517 ,'kpbhattacharya@folksinger.com', GS782563, 'Divorced', 46);
+INSERT INTO T11_CUSTOMER VALUES (213, 'Ryan', 'Dunn','1977-06-11 ','M','Ohio', 8927654891, 'dunn@stunts.com', FW892764, 'Single', 34);
+INSERT INTO T11_CUSTOMER VALUES (214, 'Pierre', 'Curie','1859-05-15' ,'M', 'Paris', 9830553876, 'curie@scientist.com', DA765432, 'Married', 46);
+INSERT INTO T11_CUSTOMER VALUES (215, 'Hema', 'Malini','1948-10-16' ,'F', 'Tamil Nadu', 8926778935 ,'hemamalini@actress.com', HG897643, 'Married', 72);
+INSERT INTO T11_CUSTOMER VALUES (216, 'Grace', 'Kelly','1929-11-12' ,'F','Pennsylvania', 79217654897, 'kelly@actress.com', JQ873078, 'Married', 52);
+INSERT INTO T11_CUSTOMER VALUES (217, 'Sana', 'Iqbal','1987-01-16' ,'F','Hyderabad', 7908887689 ,'sana@biker.com', A7896736, 'Single', 30);
+INSERT INTO T11_CUSTOMER VALUES (218, 'Sunil', 'Grover','1977-08-03' ,'M','Mandi', 8765997647, 'sunilgrover@actor.com', HA683757, 'Married', 37);
+INSERT INTO T11_CUSTOMER VALUES (219, 'Rajesh', 'Pilot', '1945-02-10', 'M', 'Ghaziabad', 789875783, 'rajesh@iaf.com', IA672867, 'Married', 55);
+INSERT INTO T11_CUSTOMER VALUES (220, 'Jayne', 'Mansfield','1933-04-19', 'F', 'Pennsylvania', 8975889764, 'jayne@actress.com', SD768388, 'Married', 34);
+
+
+-- table 3
+
+-- table 4
+
+INSERT INTO T11_INSURANCE_COMPANY VALUES('Dun & Bradstreet Private Limited','103 JFK Parkway, Short Hills, Millburn, New Jersey', (973) 921-5500, 08977109876, 'receivablequestions@dnb.com', 'https://www.dnb.com/','USA','Dun & Bradstree Vehicle Insurance Division', 'Dun & Bradstree Insurance');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('Reliance Life Insurance Corporation Limited','1st Floor, Atharva Building, Behind Payyade Residency, Off Mira Bhayander Road, Mira Road - East, Mumbai, Thane district', 022-28555240, 1800872555,'rnlife.customerservice@relianceada.com', 'https://www.reliancenipponlife.com/','Mumbai ','Reliance Vehicle Insurance Division','Reliance Life Insurance');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('Bupa Insurance Group','33 Exhibition Street, Melbourne, VIC 3000 Australia', 8947872827, 1800767628, 'info@bupa.com.au', 'https://www.bupa.com.au/', 'Australia', 'Bupa Vehicle Division', 'Bupa Insurance Company');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('Aviva Insurance Company Limited','Aviva plc, St Helens, 1 Undershaft, EC3P 3DQ, London', 1800897388, NULL, 'contact@aviva.co.uk', 'https://www.aviva,co.uk', 'United Kingdom', 'Aviva Vehicles Inusrance', 'Aviva Insurances');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('Birkshire Hathaway Inc.','Kiewit Plaza, 3555 Farnam St., Omaha, NE', 4023461400, NULL,'berkshire@berkshirehathaway.com', 'https://www.berkshirehathaway.com/', 'Nebraska', 'BH Vehicles','BH Insurance');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('The Hartford Financial Services Group, Inc.','The Hartford Financial Services Group, Inc., 690 Asylum Avenue, Hartford, CT', 8605475000, 08492145766, 'insurances@thehartford.com','https://www.thehartfors.com/','United States','Hartford Vehicles Division','The Hartford');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('Bharti AXA General Insurance Company Limited. First Floor Hosto Centre, No. 43, Millers Road, Vasanth Nagar', 1800975658, NULL,'contact@bhartiaxa.com', 'https://www.bhartiaxa.com', 'Banglore', 'Bharti Vehicles','Bharti Insurances');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('PNB Insurance Company Limited','Unit No. 701, 702 & 703, 7th Floor, West Wing, Raheja Towers, 26/27 M G Road', 1800786668, 02241790000, 'indiaservice@pnbmetlife.co.in','https://www.pnbmetlife.co.in/', 'Mumbai','PNB Vehicles Division','PNB Insurances');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('AAR Insurance, Kenya','Real Tower, Upperhill, Kenya', 18006757383, NULL, 'info@aar.co.ke', 'https://www.aar.co.ke', 'Nairobi', 'AAR Veh Ins', 'AAR Insurance Solutions');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('American International Group, Inc.', '175, Water Street, New York', 1800498592, 0236645788,'info@aig.com', 'https://www.aig.com/', 'New York','AIG Vehicle Solutions', 'AIG Insurances');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('SBI Insurance Company Limited', 'SBI Corporate Solutions. 4th Floor, MV Road, Andheri (East)', 1899983766, NULL, 'info@sbiinsurance.com', 'https://www.sbiinsurance.com/', 'Mumbai', 'SBI Vehicle Solutions', 'SBI Insurance Company');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('Tata Insurance Company Limited', 'Tower A, Peninsula Business Park, Senapati Bapat Marg, Lower Parel', 1088997755, 0234888829,'info@tata.com', 'https://www.tata.com', 'Mumbai', 'Tata Vehicle Insurances','Tata Insurances');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('Reliance Vehicle Insurance Company Limited','Reliance Centre, 5th floor, Off Western Express Highway, Santacruz East', 1899007777, NULL, 'info@reliancelife.com','https://www.reliancelife.com', 'Mumbai','Reliance Vehicles','Reliance Solutions');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('Aegon Insurance Company Limited','2nd Floor, Aditya Trade Center, 7-1-618, Hyderabad', 1800977678, NULL, 'help@aegon.com','https://www.aegon.com/', 'Hyderabad','Aegon Vehicle Solutions','Aegon Insurances');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('Sharma Insurance Company Limited','4th Floor, Commerce Enclave, Financial District, Gachibowli', 1800976749, NULL, 'info@sharmainsurances.com','https://www.shramainsurances.com', 'Hyderabad','Sharma Insurances','Sharma Solutions');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('DP International Vehicle Insurance Limited','3rd Floor, RajMandir Bhavan, Begumpet', 1800678692, NULL, 'info@dpinternational.com', 'https://www.dpinternational.com', 'Kolkata','DP Vehicle Insurance','DP Insurances');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('DLF Pramerica Insurance Company Limited','5th floor, Bezzola Complex Ltd., Sion Trombay Road,', 18000784677, NULL, 'info@dlfpi.com','https://www.dlfpi.com/', 'Sion','DLF Vehicle Division', 'DLF Insurances');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('IDBI Insurance Company Limited','Office Building Near Narayanaguda Flyover, Hyderabad, Telangana', 1866798288, NULL,'contact@idbi.com', 'https://www.idbi.com/', 'Hyderabad','IDBI Vehicles Division','IDBI Insurace Office');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('Vehicles Association of India','5th Floor, VAI Complex, Industrial Estate, Mumbai', 18099489288, 028887927,'info@vai.com','https://www.vai.com','Mumbai','VAI Vehicles','VAI Solutions');
+INSERT INTO T11_INSURANCE_COMPANY VALUES('Life Insurance Corporation of India Limited','3rd Floor, BSE Building, New Okhla Industrial Development Authority', 1804489999, NULL, 'info@lic.com','https://www.lic.com', 'Noida','LIC Vehicle Insurance','LIC Office');
+
+-- table 5
+INSERT INTO T11_DEPARTMENT VALUES
+     ('Dun & Bradstree Vehicle Insurance Division', '501', 'Kendra Castillo', 'Dun & Bradstreet Private Limited'),
+     ('Reliance Vehicle Insurance Division', '502', 'Elana Tyson', 'Reliance Life Insurance Corporation Limited'),
+     ('Bupa Vehicle Division', '503', 'Hawa Ferrell', 'Bupa Insurance Group'),
+     ('Aviva Vehicles Inusrance', '504', 'Addie Estes', 'Aviva Insurance Company Limited'),
+     ('BH Vehicles', '505', 'Pranav Gupta', 'Birkshire Hathaway Inc.'),
+     ('Hartford Vehicles Division', '506', 'Cinar Ratcliffe', 'The Hartford Financial Services Group'),
+     ('Bharti Vehicles', '507', 'Elara Doyle','Bharti AXA General Insurance Company Limited'),
+     ('PNB Vehicles Division', '508', 'Christine Smith', 'PNB Insurance Company Limited'),
+     ('AAR Veh Ins', '509', 'Benas Vinson', 'AAR Insurance, Kenya'),
+     ('AIG Vehicle Solutions', '510', 'Anabella Dickson','American International Group, Inc.'),
+     ('SBI Vehicle Solutions', '511', 'Arnav Singh', 'SBI Insurance Company Limited'),
+     ('Tata Vehicle Insurances', '512', 'Sharon Lyon', 'Tata Insurance Company Limited'),
+     ('Reliance Vehicles', '513', 'Malikah Frederick', 'Reliance Vehicle Insurance Company Limited'),
+     ('Aegon Vehicle Solutions', '514', 'Connah Knight', 'Aegon Insurance Company Limited'),
+     ('Sharma Insurances', '515', 'Jagoda Copeland', 'Sharma Insurance Company Limited'),
+     ('DP Vehicle Insurance', '516', 'Kylo Vang', 'DP International Vehicle Insurance Limited'),
+     ('DLF Vehicle Division', '517', 'Kobi Stott', 'DLF Pramerica Insurance Company Limited'),
+     ('IDBI Vehicles Division', '518', 'Naseem Coleman', 'IDBI Insurance Company Limited'),
+	('VAI Vehicles', '519', 'Angelo Kouma', 'Vehicles Association of India'),
+     ('LIC Vehicle Insurance', '520', 'Jo Schultz', 'Life Insurance Corporation of India Limited'),
+
+-- table 6
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('Reliance Vehicle Insurance Division','AB','Valencia','9933786475','Kendra Castillo','part-specific-service','502', ' Reliance Life Insurance Corporation Limited');
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('Hartford Vehicles Division','CD','Shahkot','8977476654', 'Cinar Ratcliffe','full-service','506','The Hartford Financial Services Group, Inc.');
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('Bharti Vehicles','EF','Paris','9088997266','Elara Doyle','minor-service','507',  'Bharti AXA General Insurance Company Limited');
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('SBI Vehicle Solutions','GH','Germany','9083686266','Arnav Singh','part-specific-service','511', 'SBI Insurance Company Limited');
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('Reliance Vehicles','IJ','California','9098287767', 'Malikah Frederick','full-service','513',  'Reliance Vehicle Insurance Company Limited');
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('PNB Vehicles Division','KL','Dallas','9093874666','Christine Smith','half-service','508',  'PNB Insurance Company Limited');
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('Bupa Vehicle Division','MN','California','9093874666', 'Hawa Ferrell','full-major-service','503', 'Bupa Insurance Group');
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('Dun & Bradstree Vehicle Insurance Division','OP','Mumbai','9090938888', 'Kendra Castillo','half-service','501','Dun & Bradstreet Private Limited');
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('Aviva Vehicles Inusrance','QR','Nairobi','8902667477','Addie Estes','partial-service','504',  'Aviva Insurance Company Limited');
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('DLF Vehicle Division','ST','California','8978889978','Kobi Stott','full-service','517', ' DLF Pramerica Insurance Company Limited');
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('IDBI Vehicles Division','UV','Taiwan','9093875995','Naseem Coleman','partial-service','518','IDBI Insurance Company Limited');
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('VAI Vehicles','WX','Hooghly','9094978577','Angelo Kouma','minor-service','519',  'IDBI Federal Vehicle Insurance Company Limited');
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('Tata Vehicle Insurances','YZ','Pennsylvania','9095999954','Sharon Lyon','major-service','512', 'Tata Insurance Company Limited');
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('AAR Veh Ins','AA','Paris', '9094877758','Benas Vinson','full-service','509',  'AAR Insurance, Kenya');
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('AIG Vehicle Solutions','BB','Mumbai','9093875766', 'Anabella Dickson','half-service','510', 'American International Group, Inc.');
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('Aegon Vehicle Solutions','CC','Monaco','9093875893', 'Connah Knight', 'part-specific-service','514', 'Aegon Insurance Company Limited');
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('DP Vehicle Insurance','DD','Hyderabad','9080949898','Kylo Vang', 'full-service','516', 'DP International Vehicle Insurance Limited');
+INSERT INTO T11_VEHICLE_SERVICE VALUES ('Sharma Insurances','EE','Lousiana','8989488849', 'Jagoda Copeland','minor-service','515', 'Sharma Insurance Company Limited');
